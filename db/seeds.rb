e3db1ab153387ba5f27456e9f3f10b4c46c74b29
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-04 22:17:31 -0400

    updated seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,33 +4,27 @@ puts "🌱 Seeding spices..."
 
 User.create(username: "tony", password: "1450575459", profile_img: "https://cdn.vox-cdn.com/thumbor/AaMw8bQMRhPOeiTy-ldtOKt9nfc=/0x38:1920x1043/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/23601546/diabloimmortal.png") #123456
 User.create(username: "derek", password: "92599395", profile_img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSrBpTMNtfNhdBZVYuyad_lzcD0C3vKeiy1oA&usqp=CAU") #abcde
-User.create(username: "alan", password: "1724788940") #iforget
+
+Relation.create(user: User.first, friend_id: User.second.id, invited_by: User.first.id, status: "accepted")
+Relation.create(user: User.second, friend_id: User.first.id, invited_by: User.first.id, status: "accepted")
 
 
 Game.create(title: "Tic Tac Toe", image_url: "https://m.media-amazon.com/images/I/61kjGo7vPtL._AC_SX679_.jpg", description: "X O X O X" )
 Game.create(title: "Collect 4", image_url: "https://www.ultraboardgames.com/connect4/gfx/banner2.jpg" )
 Game.create(title: "Reversi", image_url: "https://playpager.com/wp-content/uploads/2019/08/reversi-game.jpg" )
-Game.create(title: "Monopoly", image_url: "https://m.media-amazon.com/images/I/81oC5pYhh2L._AC_SL1500_.jpg", description: "You might not be able see it on this website in your life time~" )
-
-Match.create(game: Game.first)
-
-UsersMatch.create(match: Match.first, user: User.first,  friend_id:User.second.id, invited_by:User.first.id)
-UsersMatch.create(match: Match.first, user: User.second, friend_id:User.first.id, invited_by:User.first.id)
+Game.create(title: "Monopoly", image_url: "https://static.wixstatic.com/media/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg/v1/fill/w_1800,h_1800,al_c,q_85/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg", description: "You might not be able see it on this website in your life time~" )
 
 
-Chat.create()
-UsersChat.create(chat: Chat.first, user: User.first)
-UsersChat.create(chat: Chat.first, user: User.second)
+# Chat.create()
+# UsersChat.create(chat: Chat.first, user: User.first)
+# UsersChat.create(chat: Chat.first, user: User.second)
 
-Message.create(chat: Chat.first, user: User.first, message: "Lets play a game Derek.")
-Message.create(chat: Chat.first, user: User.second, message: "No, I want to go to concert.")
 
-Relation.create(user: User.first, friend_id: User.second.id, invited_by: User.first.id, status: "accepted")
-Relation.create(user: User.second, friend_id: User.first.id, invited_by: User.first.id, status: "accepted")
-Relation.create(user: User.first, friend_id: User.third.id, invited_by: User.first.id, status: "accepted")
-Relation.create(user: User.third, friend_id: User.first.id, invited_by: User.first.id, status: "accepted")
-Relation.create(user: User.second, friend_id: User.last.id, invited_by: User.second.id, status: "accepted")
-Relation.create(user: User.last, friend_id: User.second.id, invited_by: User.first.id, status: "accepted")
+# Message.create(chat: Chat.first, user: User.first, message: "Lets play a game Derek.")
+# Message.create(chat: Chat.first, user: User.second, message: "No, I want to go to concert.")
+
+
+
 
 
 puts "✅ Done seeding!"
