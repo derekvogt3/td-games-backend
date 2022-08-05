93c0f77e9e86707b681fbb7fd734c12153077559
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-04 23:09:46 -0400

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ Relation.create(user: User.second, friend_id: User.first.id, invited_by: User.fi
 Game.create(title: "Tic Tac Toe", image_url: "https://m.media-amazon.com/images/I/61kjGo7vPtL._AC_SX679_.jpg", description: "X O X O X" )
 Game.create(title: "Collect 4", image_url: "https://www.ultraboardgames.com/connect4/gfx/banner2.jpg" )
 Game.create(title: "Reversi", image_url: "https://playpager.com/wp-content/uploads/2019/08/reversi-game.jpg" )
-Game.create(title: "Monopoly", image_url: "https://static.wixstatic.com/media/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg/v1/fill/w_1800,h_1800,al_c,q_85/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg", description: "You might not be able see it on this website in your life time~" )
+Game.create(title: "Monopoly", image_url: "https://static.wixstatic.com/media/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg/v1/fill/w_1800,h_1800,al_c,q_85/ec027f_6e3873ecc5a346e591919f1ef796829f~mv2_d_1800_1800_s_2.jpg", description: "You might not be able see it on this website in your lifetime~" )
 
 
 # Chat.create()
