080c2cd059028876f177110e31ee79f8d2dd88a4
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-08-10 16:17:17 -0400

    added dependent: :destroy to Match and Chat Model

--- app/models/Chat.rb
+++ app/models/Chat.rb
@@ -1,6 +1,6 @@
 class Chat < ActiveRecord::Base
   belongs_to :match
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   has_many :users_chats
   has_many :users, through: :users_chats
